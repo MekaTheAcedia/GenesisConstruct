6b27ac2d214517817ddfc4f416ca9df9da53d351
Meka <04062002nst@gmail.com>
2018-10-15 10:26:46 +0700

    DB correction, public/index fix

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -33,10 +33,10 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 			<div class="left-side sticky-left-side">
 				<!--logo and iconic logo start-->
 				<div class="logo">
-					<h1><a href="index.html">Mosai<span>c</span></a></h1>
+					<h1><a href="../public">Mosai<span>c</span></a></h1>
 				</div>
 				<div class="logo-icon text-center">
-					<a href="index.html">M </a>
+					<a href="../public">M </a>
 				</div>
 				<!--logo and iconic logo end-->
 				<div class="left-side-inner">
@@ -232,58 +232,58 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!-- //header-ends -->
 @yield('content')
 <div class="footer">
-<div class="footer-grid">
-	<h3>Navigation</h3>
-	<ul class="list1">
-		<li><a href="index.html">Home</a></li>
-		<li><a href="radio">All Songs</a></li>
-		<li><a href="browse">Albums</a></li>
-		<li><a href="radio">New Collections</a></li>
-		<li><a href="blog">Blog</a></li>
-		<li><a href="contact">Contact</a></li>
-	</ul>
-</div>
-<div class="footer-grid">
-	<h3>Our Account</h3>
-	<ul class="list1">
-		<li><a href="#" data-toggle="modal" data-target="#myModal5">Your Account</a></li>
-		<li><a href="#">Personal information</a></li>
-		<li><a href="#">Addresses</a></li>
-		<li><a href="#">Discount</a></li>
-		<li><a href="#">Orders history</a></li>
-		<li><a href="#">Addresses</a></li>
-		<li><a href="#">Search Terms</a></li>
-	</ul>
-</div>
-<div class="footer-grid">
-	<h3>Our Support</h3>
-	<ul class="list1">
-		<li><a href="contact">Site Map</a></li>
-		<li><a href="#">Search Terms</a></li>
-		<li><a href="#">Advanced Search</a></li>
-		<li><a href="#">Mobile</a></li>
-		<li><a href="contact">Contact Us</a></li>
-		<li><a href="#">Mobile</a></li>
-		<li><a href="#">Addresses</a></li>
-	</ul>
-</div>
-<div class="footer-grid">
-	<h3>Newsletter</h3>
-	<p class="footer_desc">Nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat</p>
-	<div class="search_footer">
-		<form>
-			<input type="text" placeholder="Email...." required="">
-			<input type="submit" value="Submit">
-		</form>
+	<div class="footer-grid">
+		<h3>Navigation</h3>
+		<ul class="list1">
+			<li><a href="../public">Home</a></li>
+			<li><a href="radio">All Songs</a></li>
+			<li><a href="browse">Albums</a></li>
+			<li><a href="radio">New Collections</a></li>
+			<li><a href="blog">Blog</a></li>
+			<li><a href="contact">Contact</a></li>
+		</ul>
 	</div>
-</div>
-<div class="footer-grid footer-grid_last">
-	<h3>About Us</h3>
-	<p class="footer_desc">Diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat enim ad minim veniam,.</p>
-	<p class="f_text">Phone:  &nbsp;&nbsp;&nbsp;00-250-2131</p>
-	<p class="email">Email : &nbsp;<span><a href="mailto:mail@example.com">info(at)mailing.com</a></span></p>
-</div>
-<div class="clearfix"> </div>
+	<div class="footer-grid">
+		<h3>Our Account</h3>
+		<ul class="list1">
+			<li><a href="#" data-toggle="modal" data-target="#myModal5">Your Account</a></li>
+			<li><a href="#">Personal information</a></li>
+			<li><a href="#">Addresses</a></li>
+			<li><a href="#">Discount</a></li>
+			<li><a href="#">Orders history</a></li>
+			<li><a href="#">Addresses</a></li>
+			<li><a href="#">Search Terms</a></li>
+		</ul>
+	</div>
+	<div class="footer-grid">
+		<h3>Our Support</h3>
+		<ul class="list1">
+			<li><a href="contact">Site Map</a></li>
+			<li><a href="#">Search Terms</a></li>
+			<li><a href="#">Advanced Search</a></li>
+			<li><a href="#">Mobile</a></li>
+			<li><a href="contact">Contact Us</a></li>
+			<li><a href="#">Mobile</a></li>
+			<li><a href="#">Addresses</a></li>
+		</ul>
+	</div>
+	<div class="footer-grid">
+		<h3>Newsletter</h3>
+		<p class="footer_desc">Nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat</p>
+		<div class="search_footer">
+			<form>
+				<input type="text" placeholder="Email...." required="">
+				<input type="submit" value="Submit">
+			</form>
+		</div>
+	</div>
+	<div class="footer-grid footer-grid_last">
+		<h3>About Us</h3>
+		<p class="footer_desc">Diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat enim ad minim veniam,.</p>
+		<p class="f_text">Phone:  &nbsp;&nbsp;&nbsp;00-250-2131</p>
+		<p class="email">Email : &nbsp;<span><a href="mailto:mail@example.com">info(at)mailing.com</a></span></p>
+	</div>
+	<div class="clearfix"> </div>
 </div>
 </div>
 <!--footer section start-->
--- resources/views/typography.blade.php
+++ resources/views/typography.blade.php
@@ -409,243 +409,255 @@
 							<input type="checkbox" aria-label="...">
 						</span>
 						<input type="text" class="form-control" aria-label="...">
-						</div><!-- /input-group -->
-						</div><!-- /.col-lg-6 -->
-						<div class="col-lg-6 in-gp-tb">
-							<div class="input-group">
-								<span class="input-group-addon">
-									<input type="radio" aria-label="...">
-								</span>
-								<input type="text" class="form-control" aria-label="...">
-								</div><!-- /input-group -->
-								</div><!-- /.col-lg-6 -->
-								</div><!-- /.row -->
-								<div class="row">
-									<div class="col-lg-6 in-gp-tl">
-										<div class="input-group">
-											<span class="input-group-btn">
-												<button class="btn btn-default" type="button">Go!</button>
-											</span>
-											<input type="text" class="form-control" placeholder="Search for...">
-										</div>
-										<!-- /input-group -->
-									</div>
-									<!-- /.col-lg-6 -->
-									<div class="col-lg-6 in-gp-tb">
-										<div class="input-group">
-											<input type="text" class="form-control" placeholder="Search for...">
-											<span class="input-group-btn">
-												<button class="btn btn-default" type="button">Go!</button>
-											</span>
-										</div>
-										<!-- /input-group -->
-									</div>
-									<!-- /.col-lg-6 -->
-								</div>
-								<!-- /.row -->
-								<div class="row">
-									<div class="col-lg-6 in-gp-tl">
-										<div class="input-group">
-											<div class="input-group-btn">
-												<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Action <span class="caret"></span></button>
-												<ul class="dropdown-menu">
-													<li><a href="#">Action</a></li>
-													<li><a href="#">Another action</a></li>
-													<li><a href="#">Something else here</a></li>
-													<li role="separator" class="divider"></li>
-													<li><a href="#">Separated link</a></li>
-												</ul>
-												</div><!-- /btn-group -->
-												<input type="text" class="form-control" aria-label="...">
-												</div><!-- /input-group -->
-												</div><!-- /.col-lg-6 -->
-												<div class="col-lg-6 in-gp-tb">
-													<div class="input-group">
-														<input type="text" class="form-control" aria-label="...">
-														<div class="input-group-btn">
-															<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Action <span class="caret"></span></button>
-															<ul class="dropdown-menu dropdown-menu-right">
-																<li><a href="#">Action</a></li>
-																<li><a href="#">Another action</a></li>
-																<li><a href="#">Something else here</a></li>
-																<li role="separator" class="divider"></li>
-																<li><a href="#">Separated link</a></li>
-															</ul>
-															</div><!-- /btn-group -->
-															</div><!-- /input-group -->
-															</div><!-- /.col-lg-6 -->
-															</div><!-- /.row -->
-															<!-- /wthree-agile -->
-															<h3 class="typo">Grids</h3>
-															<div class="form-group">
-																<div class="row">
-																	<div class="col-md-12">
-																		<input type="text" class="form-control1" placeholder=".col-md-12">
-																	</div>
-																	<div class="clearfix"> </div>
-																</div>
-															</div>
-															<div class="form-group">
-																<div class="row">
-																	<div class="col-md-2 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-2">
-																	</div>
-																	<div class="col-md-10">
-																		<input type="text" class="form-control1" placeholder=".col-md-10">
-																	</div>
-																	<div class="clearfix"> </div>
-																</div>
-															</div>
-															<div class="form-group">
-																<div class="row">
-																	<div class="col-md-3 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-3">
-																	</div>
-																	<div class="col-md-9">
-																		<input type="text" class="form-control1" placeholder=".col-md-9">
-																	</div>
-																	<div class="clearfix"> </div>
-																</div>
-															</div>
-															<div class="form-group">
-																<div class="row">
-																	<div class="col-md-4 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-4">
-																	</div>
-																	<div class="col-md-8">
-																		<input type="text" class="form-control1" placeholder=".col-md-8">
-																	</div>
-																	<div class="clearfix"> </div>
-																</div>
-															</div>
-															<div class="form-group">
-																<div class="row">
-																	<div class="col-md-5 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-5">
-																	</div>
-																	<div class="col-md-7">
-																		<input type="text" class="form-control1" placeholder=".col-md-7">
-																	</div>
-																	<div class="clearfix"> </div>
-																</div>
-															</div>
-															<div class="form-group">
-																<div class="row">
-																	<div class="col-md-6 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-6">
-																	</div>
-																	<div class="col-md-6">
-																		<input type="text" class="form-control1" placeholder=".col-md-6">
-																	</div>
-																	<div class="clearfix"> </div>
-																</div>
-															</div>
-															<div class="form-group">
-																<div class="row">
-																	<div class="col-md-4 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-4">
-																	</div>
-																	<div class="col-md-4 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-4">
-																	</div>
-																	<div class="col-md-4">
-																		<input type="text" class="form-control1" placeholder=".col-md-4">
-																	</div>
-																	<div class="clearfix"> </div>
-																</div>
-															</div>
-															<div class="form-group">
-																<div class="row">
-																	<div class="col-md-3 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-3">
-																	</div>
-																	<div class="col-md-6 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-6">
-																	</div>
-																	<div class="col-md-3">
-																		<input type="text" class="form-control1" placeholder=".col-md-3">
-																	</div>
-																	<div class="clearfix"> </div>
-																</div>
-															</div>
-															<div class="form-group">
-																<div class="row">
-																	<div class="col-md-2 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-2">
-																	</div>
-																	<div class="col-md-8 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-8">
-																	</div>
-																	<div class="col-md-2">
-																		<input type="text" class="form-control1" placeholder=".col-md-2">
-																	</div>
-																	<div class="clearfix"> </div>
-																</div>
-															</div>
-															<div class="form-group">
-																<div class="row">
-																	<div class="col-md-3 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-3">
-																	</div>
-																	<div class="col-md-3 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-3">
-																	</div>
-																	<div class="col-md-3 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-3">
-																	</div>
-																	<div class="col-md-3">
-																		<input type="text" class="form-control1" placeholder=".col-md-3">
-																	</div>
-																	<div class="clearfix"> </div>
-																</div>
-															</div>
-															<div class="form-group">
-																<div class="row">
-																	<div class="col-md-5 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-5">
-																	</div>
-																	<div class="col-md-2 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-2">
-																	</div>
-																	<div class="col-md-5">
-																		<input type="text" class="form-control1" placeholder=".col-md-5">
-																	</div>
-																	<div class="clearfix"> </div>
-																</div>
-															</div>
-															<div class="form-group mb-n">
-																<div class="row">
-																	<div class="col-md-2 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-2">
-																	</div>
-																	<div class="col-md-2 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-2">
-																	</div>
-																	<div class="col-md-2 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-2">
-																	</div>
-																	<div class="col-md-2 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-2">
-																	</div>
-																	<div class="col-md-2 grid_box1">
-																		<input type="text" class="form-control1" placeholder=".col-md-2">
-																	</div>
-																	<div class="col-md-2">
-																		<input type="text" class="form-control1" placeholder=".col-md-2">
-																	</div>
-																	<div class="clearfix"> </div>
-																</div>
-															</div>
-														</div>
-													</div>
-													<!--//inner-content-->
-													<!--//page-wrapper-->
-												</div>
-												<!--body wrapper start-->
-											</div>
-											<div class="clearfix"></div>
-											<!--body wrapper end-->
-										</div>
-										<!-- /w3layouts-agile -->
-										<!--body wrapper end-->
-										@stsop
+					</div>
+					<!-- /input-group -->
+				</div>
+				<!-- /.col-lg-6 -->
+				<div class="col-lg-6 in-gp-tb">
+					<div class="input-group">
+						<span class="input-group-addon">
+							<input type="radio" aria-label="...">
+						</span>
+						<input type="text" class="form-control" aria-label="...">
+					</div>
+					<!-- /input-group -->
+				</div>
+				<!-- /.col-lg-6 -->
+			</div>
+			<!-- /.row -->
+			<div class="row">
+				<div class="col-lg-6 in-gp-tl">
+					<div class="input-group">
+						<span class="input-group-btn">
+							<button class="btn btn-default" type="button">Go!</button>
+						</span>
+						<input type="text" class="form-control" placeholder="Search for...">
+					</div>
+					<!-- /input-group -->
+				</div>
+				<!-- /.col-lg-6 -->
+				<div class="col-lg-6 in-gp-tb">
+					<div class="input-group">
+						<input type="text" class="form-control" placeholder="Search for...">
+						<span class="input-group-btn">
+							<button class="btn btn-default" type="button">Go!</button>
+						</span>
+					</div>
+					<!-- /input-group -->
+				</div>
+				<!-- /.col-lg-6 -->
+			</div>
+			<!-- /.row -->
+			<div class="row">
+				<div class="col-lg-6 in-gp-tl">
+					<div class="input-group">
+						<div class="input-group-btn">
+							<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Action <span class="caret"></span></button>
+							<ul class="dropdown-menu">
+								<li><a href="#">Action</a></li>
+								<li><a href="#">Another action</a></li>
+								<li><a href="#">Something else here</a></li>
+								<li role="separator" class="divider"></li>
+								<li><a href="#">Separated link</a></li>
+							</ul>
+						</div>
+						<!-- /btn-group -->
+						<input type="text" class="form-control" aria-label="...">
+					</div>
+					<!-- /input-group -->
+				</div>
+				<!-- /.col-lg-6 -->
+				<div class="col-lg-6 in-gp-tb">
+					<div class="input-group">
+						<input type="text" class="form-control" aria-label="...">
+						<div class="input-group-btn">
+							<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Action <span class="caret"></span></button>
+							<ul class="dropdown-menu dropdown-menu-right">
+								<li><a href="#">Action</a></li>
+								<li><a href="#">Another action</a></li>
+								<li><a href="#">Something else here</a></li>
+								<li role="separator" class="divider"></li>
+								<li><a href="#">Separated link</a></li>
+							</ul>
+						</div>
+						<!-- /btn-group -->
+					</div>
+					<!-- /input-group -->
+				</div>
+				<!-- /.col-lg-6 -->
+			</div>
+			<!-- /.row -->
+			<!-- /wthree-agile -->
+			<h3 class="typo">Grids</h3>
+			<div class="form-group">
+				<div class="row">
+					<div class="col-md-12">
+						<input type="text" class="form-control1" placeholder=".col-md-12">
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<div class="col-md-2 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-2">
+					</div>
+					<div class="col-md-10">
+						<input type="text" class="form-control1" placeholder=".col-md-10">
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<div class="col-md-3 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-3">
+					</div>
+					<div class="col-md-9">
+						<input type="text" class="form-control1" placeholder=".col-md-9">
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<div class="col-md-4 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-4">
+					</div>
+					<div class="col-md-8">
+						<input type="text" class="form-control1" placeholder=".col-md-8">
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<div class="col-md-5 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-5">
+					</div>
+					<div class="col-md-7">
+						<input type="text" class="form-control1" placeholder=".col-md-7">
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<div class="col-md-6 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-6">
+					</div>
+					<div class="col-md-6">
+						<input type="text" class="form-control1" placeholder=".col-md-6">
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<div class="col-md-4 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-4">
+					</div>
+					<div class="col-md-4 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-4">
+					</div>
+					<div class="col-md-4">
+						<input type="text" class="form-control1" placeholder=".col-md-4">
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<div class="col-md-3 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-3">
+					</div>
+					<div class="col-md-6 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-6">
+					</div>
+					<div class="col-md-3">
+						<input type="text" class="form-control1" placeholder=".col-md-3">
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<div class="col-md-2 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-2">
+					</div>
+					<div class="col-md-8 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-8">
+					</div>
+					<div class="col-md-2">
+						<input type="text" class="form-control1" placeholder=".col-md-2">
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<div class="col-md-3 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-3">
+					</div>
+					<div class="col-md-3 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-3">
+					</div>
+					<div class="col-md-3 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-3">
+					</div>
+					<div class="col-md-3">
+						<input type="text" class="form-control1" placeholder=".col-md-3">
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<div class="col-md-5 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-5">
+					</div>
+					<div class="col-md-2 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-2">
+					</div>
+					<div class="col-md-5">
+						<input type="text" class="form-control1" placeholder=".col-md-5">
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<div class="form-group mb-n">
+				<div class="row">
+					<div class="col-md-2 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-2">
+					</div>
+					<div class="col-md-2 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-2">
+					</div>
+					<div class="col-md-2 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-2">
+					</div>
+					<div class="col-md-2 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-2">
+					</div>
+					<div class="col-md-2 grid_box1">
+						<input type="text" class="form-control1" placeholder=".col-md-2">
+					</div>
+					<div class="col-md-2">
+						<input type="text" class="form-control1" placeholder=".col-md-2">
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--//inner-content-->
+	<!--//page-wrapper-->
+</div>
+<!--body wrapper start-->
+</div>
+<div class="clearfix"></div>
+<!--body wrapper end-->
+</div>
+<!-- /w3layouts-agile -->
+<!--body wrapper end-->
+@stsop
