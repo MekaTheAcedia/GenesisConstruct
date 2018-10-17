da1637ccac7279c5d1b75b41c1b447528e87da20
Meka <04062002nst@gmail.com>
2018-10-17 10:37:54 +0700

    login button bug fix

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -144,7 +144,6 @@
 	    var button = $('#loginButton');
 	    var box = $('#loginBox');
 	    var form = $('#loginForm');
-	    button.removeAttr('href');
 	    button.mouseup(function(login) {
 	        box.toggle();
 	        button.toggleClass('active');
