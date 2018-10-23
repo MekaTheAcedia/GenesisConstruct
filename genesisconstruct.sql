b57a24594ebe26a99e898e8348eb29d5d729518e
silentlove995 <silentlove9995@gmail.com>
2018-10-23 14:15:59 +0700

    update database

--- genesisconstruct.sql
+++ genesisconstruct.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 16, 2018 at 07:16 AM
+-- Generation Time: Oct 23, 2018 at 09:13 AM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.2.7
 
@@ -74,7 +74,10 @@ CREATE TABLE `migrations` (
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1);
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2018_10_16_091144_create_admins_table', 2),
+(4, '2018_10_18_010616_create_roles_table', 3),
+(5, '2018_10_18_010743_create_role_user_table', 3);
 
 -- --------------------------------------------------------
 
@@ -161,7 +164,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `dob`, `gender`, `location`, `about`, `level`, `avatar`, `favorite`) VALUES
-(1, 'Ly Trong', 'trongdaigia19955@gmail.com', NULL, '$2y$10$nYIaHY26PX5wEQYEuAmZmeeAuq0M9sMcjGju0bsDmMl5uNYFG5iIW', NULL, '2018-10-15 22:14:48', '2018-10-15 22:14:48', NULL, NULL, NULL, NULL, 3, NULL, NULL);
+(3, 'Lý Quốc Trọng', 'trongdaigia19955@gmail.com', NULL, '$2y$10$QOcDzOpejqMgHmKLP4agF.lvG6o.nFoPv1BzwbycqUIXmQJuuWoDC', NULL, '2018-10-22 20:17:43', '2018-10-22 20:17:43', NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -198,8 +201,8 @@ ALTER TABLE `albums`
 --
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`commentid`),
-  ADD KEY `song_comment` (`songid`),
-  ADD KEY `userid` (`userid`);
+  ADD KEY `userid` (`userid`),
+  ADD KEY `song_comment` (`songid`);
 
 --
 -- Indexes for table `migrations`
@@ -246,16 +249,46 @@ ALTER TABLE `vocals`
 --
 
 --
+-- AUTO_INCREMENT for table `albums`
+--
+ALTER TABLE `albums`
+  MODIFY `albumid` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `commentid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `producer`
+--
+ALTER TABLE `producer`
+  MODIFY `producerid` int(15) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `songs`
+--
+ALTER TABLE `songs`
+  MODIFY `songid` int(15) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `vocals`
+--
+ALTER TABLE `vocals`
+  MODIFY `vocalid` int(15) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -265,7 +298,7 @@ ALTER TABLE `users`
 -- Constraints for table `albums`
 --
 ALTER TABLE `albums`
-  ADD CONSTRAINT `producer_connect` FOREIGN KEY (`producerid`) REFERENCES `producer` (`producerid`);
+  ADD CONSTRAINT `producer_name` FOREIGN KEY (`producerid`) REFERENCES `producer` (`producerid`);
 
 --
 -- Constraints for table `comments`
@@ -280,7 +313,7 @@ ALTER TABLE `comments`
 ALTER TABLE `songs`
   ADD CONSTRAINT `album_connect` FOREIGN KEY (`albumid`) REFERENCES `albums` (`albumid`),
   ADD CONSTRAINT `producer_connect_song` FOREIGN KEY (`producerid`) REFERENCES `producer` (`producerid`),
-  ADD CONSTRAINT `vocal_connect` FOREIGN KEY (`vocalid`) REFERENCES `vocals` (`vocalid`);
+  ADD CONSTRAINT `vocal_name` FOREIGN KEY (`vocalid`) REFERENCES `vocals` (`vocalid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
